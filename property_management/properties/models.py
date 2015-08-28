5fdf4759eb451b491332c521932b545e99767a52
Jordan Anslow <jordananslow@gmail.com>
2015-08-28 04:06:58 +0000

    Added image field to property model

--- property_management/properties/models.py
+++ property_management/properties/models.py
@@ -1,3 +1,24 @@
+"""
+Provides models from which Django will generate the database
+
+Primary key automatically provided by Django for each class
+in the form: id = models.AutoField(primary_key=True)
+"""
+
 from django.db import models
 
 # Create your models here.
+class Property(models.Model):
+    """
+    Model for an individual property
+    """
+    address = models.CharField(max_length=255)
+    description = models.TextField()
+    pets_allowed = models.BooleanField()
+    contact_information = models.TextField() # refactor into JSON field (maybe)
+    # simple link to an imgur file, if no input set to blank field
+    image = models.CharField(max_length=128, blank=True, default='')
+
+    # Identifies self as address name rather than 'Property Object'
+    def __str__(self):
+        return self.address
