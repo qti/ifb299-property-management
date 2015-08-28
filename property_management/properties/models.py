109cd2bc756463ad2f885b23a72fd71ea2ea7bca
Jordan Anslow <jordananslow@gmail.com>
2015-08-28 07:52:48 +0000

    Added tests for property id's

--- property_management/properties/models.py
+++ property_management/properties/models.py
@@ -14,7 +14,7 @@ class Property(models.Model):
     """
     address = models.CharField(max_length=255)
     description = models.TextField()
-    pets_allowed = models.BooleanField()
+    pets_allowed = models.BooleanField(default=False)
     contact_information = models.TextField() # refactor into JSON field (maybe)
     # simple link to an imgur file, if no input set to blank field
     image = models.CharField(max_length=128, blank=True, default='')
--- property_management/properties/tests.py
+++ property_management/properties/tests.py
@@ -1,3 +1,60 @@
+"""
+Contains unit tests the properties app
+"""
 from django.test import TestCase
 
+from .models import Property
+
+# def setUp(self):
+#     self.test_property_one = Property.objects.create(
+#         address = "27 Foo, dev null place, 1337",
+#         pets_allowed = False,
+#         contact_information = " { 'test': field, 'another': field } ",
+#     )
+
+#     self.test_property_two = Property.objects.create(
+#         address = "99 Foo, dev null place, 0000",
+#         description = "Foo foo, foo; foo, foo; foo!",
+#         contact_information = " { 'test': field, 'another': field } ",
+#     )
+
+#     self.test_property_three = Property.objects.create(
+#         address = "42 Foo, dev null place, 9999",
+#         description = "Foo foo, foo; foo, foo; foo!",
+#         pets_allowed = False,
+#     )
+
+#     self.test_property_four = Property.objects.create(
+#         address = "356 Foo, dev null place, 12345",
+#         description = "Foo foo, foo; foo, foo; foo!",
+#         pets_allowed = False,
+#         contact_information = " { 'test': field, 'another': field } ",
+#     )
+
 # Create your tests here.
+class PropertyModelTests(TestCase):
+    def test_id_creation(self):
+        test_property_one = Property.objects.create(
+            address="27 Foo, dev null place, 1337",
+            pets_allowed=False,
+            contact_information=" { 'test': field, 'another': field } ",
+        )
+
+        properties = Property.objects.get(pk=1)
+        self.assertEqual(test_property_one, properties)
+
+    def test_id_iteration(self):
+        test_property_one = Property.objects.create(
+            address="27 Foo, dev null place, 1337",
+            pets_allowed=False,
+            contact_information=" { 'test': field, 'another': field } ",
+        )
+
+        test_property_two = Property.objects.create(
+            address="99 Foo, dev null place, 0000",
+            description="Foo foo, foo; foo, foo; foo!",
+            contact_information=" { 'test': field, 'another': field } ",
+        )
+
+        properties = Property.objects.all().count()
+        self.assertEqual(properties, Property.objects.get(pk=2).id)
