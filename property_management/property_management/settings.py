0d88e57ae529b8aae67213fb4f31fa77de97cc05
Jordan Anslow <jordananslow@gmail.com>
2015-09-02 11:52:32 +0000

    Heroku update

--- property_management/property_management/settings.py
+++ property_management/property_management/settings.py
@@ -59,7 +59,10 @@ TEMPLATE_DIRS = (
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': [
+            'templates',
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
