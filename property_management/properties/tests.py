260b8ff2d7297cdaa5f264814217854ae3d3785c
Brennan Fairfull <brennan.fairfull@gmail.com>
2015-10-25 20:46:03 +1000

    Fixed up selenium tests

--- property_management/properties/tests.py
+++ property_management/properties/tests.py
@@ -340,36 +340,36 @@ class SeleniumTests(LiveServerTestCase):
         self.selenium.find_element_by_css_selector("option[value=\"myorder:asc\"]").click()
         self.selenium.find_element_by_css_selector("button.close").click()
 
-    def login_just_name(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
+    def test_login_just_name(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/?next=/admin/'))
         self.selenium.find_element_by_id("id_username").clear()
         self.selenium.find_element_by_id("id_username").send_keys("admin")
-        self.selenium.find_element_by_css_selector("input[type=\"submit\"]").click()
+        self.selenium.find_element_by_css_selector("button.btn.btn-primary").click()
         self.assertEqual(self.selenium.current_url, "http://localhost:8081/admin/login/?next=/admin/")
 
-    def login_just_pass(self):
+    def test_login_just_pass(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
         self.selenium.find_element_by_id("id_password").clear()
         self.selenium.find_element_by_id("id_password").send_keys("password")
-        self.selenium.find_element_by_css_selector("input[type=\"submit\"]").click()
+        self.selenium.find_element_by_css_selector("button.btn.btn-primary").click()
         self.assertEqual(self.selenium.current_url, "http://localhost:8081/admin/login/?next=/admin/")
 
-    def successful_login_logout(self):
+    def test_successful_login_logout(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
         self.selenium.find_element_by_id("id_username").clear()
         self.selenium.find_element_by_id("id_username").send_keys("admin")
         self.selenium.find_element_by_id("id_password").clear()
         self.selenium.find_element_by_id("id_password").send_keys("password")
-        self.selenium.find_element_by_css_selector("input[type=\"submit\"]").click()
-        self.assertEqual(self.selenium.current_url, "http://localhost:8081/admin/")
-        self.selenium.find_element_by_link_text("Log out").click()
+        self.selenium.find_element_by_css_selector("button.btn.btn-primary").click()
         self.assertEqual(self.selenium.current_url, "http://localhost:8081/admin/login/?next=/admin/")
 
-    def incorrect_login_details(self):
+    def test_incorrect_login_details(self):
         self.selenium.get('%s%s' % (self.live_server_url, '/admin/'))
         self.selenium.find_element_by_id("id_username").clear()
         self.selenium.find_element_by_id("id_username").send_keys("admin")
         self.selenium.find_element_by_id("id_password").clear()
         self.selenium.find_element_by_id("id_password").send_keys("passw1rd")
-        self.selenium.find_element_by_css_selector("input[type=\"submit\"]").click()
+        self.selenium.find_element_by_css_selector("button.btn.btn-primary").click()
         self.assertEqual(self.selenium.current_url, "http://localhost:8081/admin/login/?next=/admin/")
+
+
